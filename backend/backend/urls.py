ace87656a417521b2b76f2aa6d2c946a38ca9412
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-02-23 18:25:12 +0400

    Create user serializers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from os import getenv
 
 api_patterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     path(getenv('ADMIN_URL'), admin.site.urls),
     path('api/', include(api_patterns)),
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
